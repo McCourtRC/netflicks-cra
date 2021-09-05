1d3cc0857fc8466e9235c22f7e7dfb7f2229f264
Corey McCourt <mccourtrc@gmail.com>
2021-09-05 12:52:51 -0700

    move TodoList to folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from 'components/Header';
-import TodoList from 'components/TodoList';
+import TodoList from 'components/TodoList/TodoList';
 import styled from '@emotion/styled';
 
 const Container = styled.div`
