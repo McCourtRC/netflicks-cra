4e394a8c12ff6923e6edae02b4129795278ee285
Corey McCourt <mccourtrc@gmail.com>
2021-09-04 23:18:26 -0700

    add todo input

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import { ThemeProvider } from '@emotion/react';
-import { Todo } from 'types/todo';
 import { theme } from 'theme';
 import Header from 'components/Header';
 import TodoList from 'components/TodoList';
-
-const mockTodos: Todo[] = [
-  { text: 'Apply' },
-  { text: 'Slay the interview' },
-  { text: 'Get Hired' },
-];
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <div className='App'>
         <Header />
-        <TodoList todos={mockTodos} />
+        <TodoList />
       </div>
     </ThemeProvider>
   );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,18 +1,33 @@
-import React, { FC } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { Todo } from 'types/todo';
 import TodoItem from 'components/TodoItem';
 import List from 'lib/List';
 
-interface Props {
-  todos: Todo[];
-}
+const TodoList = () => {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-const TodoList: FC<Props> = ({ todos }) => {
+  const addTodo = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newTodo: Todo = { text: todo };
+
+    setTodos([...todos, newTodo]);
+    setTodo('');
+  };
+
   return (
     <List>
       {todos.map((todo, index) => (
         <TodoItem key={index} todo={todo} />
       ))}
+      <form onSubmit={addTodo}>
+        <input
+          type='text'
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+      </form>
     </List>
   );
 };
