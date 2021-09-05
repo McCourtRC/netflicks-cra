9f2a795c6f07fd78118dbd7be25c23751b1b4c93
Corey McCourt <mccourtrc@gmail.com>
2021-09-05 08:47:32 -0700

    update Button styles

--- src/lib/Button.tsx
+++ src/lib/Button.tsx
@@ -1,7 +1,17 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { Color } from 'types/theme';
 
-const Button = styled.button`
-  padding: 1rem;
-`;
+const Button = styled.button(
+  ({ color = Color.primary }) => css`
+    padding: 1rem;
+    border: none;
+    background-color: ${color};
+    :hover {
+      color: white;
+      background-color: black;
+    }
+  `
+);
 
 export default Button;
