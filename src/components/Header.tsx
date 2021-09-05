9fc897f05bcbdb135d2a75d60c17b7c626b19262
Corey McCourt <mccourtrc@gmail.com>
2021-09-04 22:05:50 -0700

    use breakpoints

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ import { css, jsx, useTheme } from '@emotion/react';
 const Header = () => {
   const {
     colors: { primary },
+    breakpoints: { md },
   } = useTheme();
 
   return (
@@ -13,6 +14,9 @@ const Header = () => {
       <h1
         css={css`
           color: ${primary};
+          ${md} {
+            color: slateblue;
+          }
         `}
       >
         Net Flicks
--- src/theme.ts
+++ src/theme.ts
@@ -1,14 +1,17 @@
 import { Theme } from "@emotion/react";
+import { BreakPoints } from "types/theme";
 
-export const theme: Theme = {
-  colors: {
-    primary: '#E50914',
-  },
-  breakpoints: {
+const breakpoints: BreakPoints= {
     sm: '@media(min-width: 640px)',
     md: '@media(min-width: 768px)',
     lg: '@media(min-width: 1024px)',
     xl: '@media(min-width: 1280px)',
     xxl: '@media(min-width: 1536px)',
+  }
+
+export const theme: Theme = {
+  colors: {
+    primary: '#E50914',
   },
+  breakpoints
 };
