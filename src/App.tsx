4364ec2d590283f1d89e4377fc111bb7693028b8
Corey McCourt <mccourtrc@gmail.com>
2021-09-04 21:54:53 -0700

    set up theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+import { Todo } from 'types/todo';
+import { theme } from 'theme';
+import Header from 'components/Header';
 import TodoList from 'components/TodoList';
 
-const mockTodos = [
+const mockTodos: Todo[] = [
   { text: 'Apply' },
   { text: 'Slay the interview' },
   { text: 'Get Hired' },
@@ -9,10 +13,12 @@ const mockTodos = [
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Net Flicks</h1>
-      <TodoList todos={mockTodos} />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <Header />
+        <TodoList todos={mockTodos} />
+      </div>
+    </ThemeProvider>
   );
 };
 
