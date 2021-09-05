c8d4a7be0fd244f702890a306410d160aad5919d
Corey McCourt <mccourtrc@gmail.com>
2021-09-05 01:30:31 -0700

    Input lib component

---
+++ src/lib/Input.tsx
@@ -0,0 +1,18 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import React, { ComponentProps, HTMLProps } from 'react';
+import { css, jsx } from '@emotion/react';
+
+const Input = (props: HTMLProps<HTMLInputElement>) => {
+  return (
+    <input
+      css={css`
+        padding: 1rem;
+        list-style: none;
+      `}
+      {...props}
+    />
+  );
+};
+
+export default Input;
